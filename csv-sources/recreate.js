aec56b1c01af6502a567b4cb1d991b086bb5ffe9
Michael Stallings <stall84@gmail.com>
2022-11-27 16:55:08 -0500

    Added a few console logs and altered package json

--- csv-sources/recreate.js
+++ csv-sources/recreate.js
@@ -1,50 +1,54 @@
-const sqlite3 = require('sqlite3').verbose();
-const fs = require('fs')
+const sqlite3 = require("sqlite3").verbose();
+const fs = require("fs");
 
-const db = new sqlite3.Database(__dirname + '/../db/quotes.db');
+const db = new sqlite3.Database(__dirname + "/../db/quotes.db");
 const CSV_ATTEMPTS_MATCH = [
-    '"([^"]+)", *"([^"]+)".*',
-    '"([^"]+)", *([^,]+).*',
-    '([^,]+), *([^,]+).*',
-    '([^,]+), *"([^"]+)".*',
+  '"([^"]+)", *"([^"]+)".*',
+  '"([^"]+)", *([^,]+).*',
+  "([^,]+), *([^,]+).*",
+  '([^,]+), *"([^"]+)".*',
 ];
 const ALREADY_ADDED = [];
 
-db.serialize(() => {
-    db.run("DROP TABLE IF EXISTS quotes");
-    db.run("CREATE TABLE quotes (quote TEXT, author TEXT, tag TEXT)");
+console.log("*** RECREATE SCRIPT INITIALIZED ***");
 
-    fs.readdirSync(__dirname + "/csv").map(csvFile => {
-        db.run("BEGIN TRANSACTION;");
-        // https://nelsonslog.wordpress.com/2014/11/16/node-js-sqlite3-very-slow-bulk-inserts/
-        const insertStatement = db.prepare("INSERT INTO quotes (quote, author, tag) VALUES (?, ?, ?)");
-        fs.readFileSync(`${__dirname}/csv/${csvFile}`, 'utf8')
-            .split("\n")
-            .forEach(csvLine => {
-                for (let regexprIndex in CSV_ATTEMPTS_MATCH) {
-                    let match = csvLine.match(CSV_ATTEMPTS_MATCH[regexprIndex]);
-                    if (match && !ALREADY_ADDED.includes(match[1]) && match[1].length > 10) {
-                        insertStatement.run([
-                            match[1],
-                            match[2],
-                            csvFile.slice(0, -4)
-                        ]);
-                        ALREADY_ADDED.push(match[1]);
-                        break;
-                    }
-                }
-            });
-        insertStatement.finalize();
-        db.run("END;", () => console.log(`${csvFile} processed`));
-    });
+db.serialize(() => {
+  db.run("DROP TABLE IF EXISTS quotes");
+  db.run("CREATE TABLE quotes (quote TEXT, author TEXT, tag TEXT)");
+
+  fs.readdirSync(__dirname + "/csv").map((csvFile) => {
+    db.run("BEGIN TRANSACTION;");
+    // https://nelsonslog.wordpress.com/2014/11/16/node-js-sqlite3-very-slow-bulk-inserts/
+    const insertStatement = db.prepare(
+      "INSERT INTO quotes (quote, author, tag) VALUES (?, ?, ?)"
+    );
+    fs.readFileSync(`${__dirname}/csv/${csvFile}`, "utf8")
+      .split("\n")
+      .forEach((csvLine) => {
+        for (let regexprIndex in CSV_ATTEMPTS_MATCH) {
+          let match = csvLine.match(CSV_ATTEMPTS_MATCH[regexprIndex]);
+          if (
+            match &&
+            !ALREADY_ADDED.includes(match[1]) &&
+            match[1].length > 10
+          ) {
+            insertStatement.run([match[1], match[2], csvFile.slice(0, -4)]);
+            ALREADY_ADDED.push(match[1]);
+            break;
+          }
+        }
+      });
+    insertStatement.finalize();
+    db.run("END;", () => console.log(`${csvFile} processed`));
+  });
 });
 
 db.run("CREATE INDEX IF NOT EXISTS tag_idx ON quotes(tag);");
 
 db.all("SELECT tag, COUNT(*) as c FROM quotes GROUP BY tag", (err, data) => {
-    console.log(data);
+  console.log(data);
 });
 
 db.get("SELECT COUNT(*) c FROM quotes", (err, row) => {
-    console.log(row.c + " records added");
+  console.log(row.c + " records added");
 });
--- index.js
+++ index.js
@@ -1,68 +1,68 @@
-const serverless = require('serverless-http');
-const express = require('express');
-const sqlite3 = require('sqlite3').verbose();
-const dbPath = __dirname + '/db/quotes.db';
-const cors = require('cors');
+const serverless = require("serverless-http");
+const express = require("express");
+const sqlite3 = require("sqlite3").verbose();
+const dbPath = __dirname + "/db/quotes.db";
+const cors = require("cors");
 
 const app = express();
 app.use(cors());
 
 // endpoints
-app.get('/tags', function (req, res) {
-    const db = new sqlite3.Database(dbPath);
+app.get("/tags", function (req, res) {
+  const db = new sqlite3.Database(dbPath);
 
-    db.all("SELECT DISTINCT tag from quotes", (err, rows) => {
-        if (err) {
-            throw new Error(err);
-        }
-        const tags = rows.map(row => row.tag);
-        res.json(tags);
+  db.all("SELECT DISTINCT tag from quotes", (err, rows) => {
+    if (err) {
+      throw new Error(err);
+    }
+    const tags = rows.map((row) => row.tag);
+    res.json(tags);
+  });
+});
+
+app.get("/quotes", function (req, res) {
+  const db = new sqlite3.Database(dbPath);
+
+  const limit = req.query.limit ? req.query.limit : 1;
+  const tag = req.query.tag ? req.query.tag : false;
+  const sql = tag
+    ? `select * FROM quotes WHERE tag="${tag}" ORDER BY random() LIMIT ${limit}`
+    : `select * FROM quotes ORDER BY random() LIMIT ${limit}`;
+
+  db.all(sql, (err, rows) => {
+    if (err) {
+      throw new Error(err);
+    }
+    const responseArray = rows.map((row) => {
+      return {
+        quote: row.quote,
+        author: row.author,
+        tag: row.tag,
+      };
     });
+    res.json(responseArray);
+  });
 });
 
-app.get('/quotes', function (req, res) {
-    const db = new sqlite3.Database(dbPath);
+app.get("/stats", function (req, res) {
+  const db = new sqlite3.Database(dbPath);
 
-    const limit = req.query.limit ? req.query.limit : 1;
-    const tag = req.query.tag ? req.query.tag : false;
-    const sql = tag ?
-        `select * FROM quotes WHERE tag="${tag}" ORDER BY random() LIMIT ${limit}`
-        : `select * FROM quotes ORDER BY random() LIMIT ${limit}`;
-
-    db.all(sql, (err, rows) => {
-        if (err) {
-            throw new Error(err);
-        }
-        const responseArray = rows.map(row => {
-            return {
-                quote: row.quote,
-                author: row.author,
-                tag: row.tag,
-            }
-        })
-        res.json(responseArray);
-    })
+  db.all("SELECT tag, COUNT(*) as c FROM quotes GROUP BY tag", (err, data) => {
+    if (err) {
+      throw new Error(err);
+    }
+    res.json(data);
+  });
 });
 
-app.get('/stats', function (req, res) {
-    const db = new sqlite3.Database(dbPath);
-
-    db.all("SELECT tag, COUNT(*) as c FROM quotes GROUP BY tag", (err, data) => {
-        if (err) {
-            throw new Error(err);
-        }
-        res.json(data);
-    });
+app.get("/", function (req, res) {
+  res.json("use /tags or /quotes");
 });
 
-app.get('/', function (req, res) {
-    res.json("use /tags or /quotes");
-});
-
-if (process.argv[2] == 'localTesting') {
-    app.listen(3000, () => {
-        console.log("Listening at http://localhost:3000");
-    });
+if (process.argv[2] == "localTesting") {
+  app.listen(3000, () => {
+    console.log("Listening at http://localhost:3000");
+  });
 } else {
-    module.exports.handler = serverless(app);
+  module.exports.handler = serverless(app);
 }
